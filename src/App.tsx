79cd7f61f9c4ef029c98a9f455fa1cf626971981
Izabelle Garcia <izabelle@udel.edu>
2025-08-29 11:18:01 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App(): React.JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
+                Izabelle Garcia was here <br />
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
